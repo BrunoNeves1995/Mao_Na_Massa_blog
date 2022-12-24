547556847f2ca198d66a72b9d90bd0e94a6ee98b
Bruno Neves <nevesbruno814@gmail.com>
2022-12-24 18:26:19 -0400

    correção dos metodos do repositorio.

---
+++ Models/Tag.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mao_Na_Massa_blog.Models
+{
+    public class Tag
+    {   
+
+        public Tag()
+        {
+            
+        }
+        
+        public Tag(int id, string name, string slug)
+        {
+            Id = id;
+            Name = name;
+            Slug = slug;
+        }
+
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Slug { get; set; }
+    }
+}
